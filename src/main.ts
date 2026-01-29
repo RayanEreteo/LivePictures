74afb401db1e3b930f4c8ccaacbb6a036c88d280
RayanEreteo <rayabf5@gmail.com>
2026-01-29 14:37:34 +0100

    Handle serverMsg text depending on success

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,22 @@ const socket = io("http://localhost:5000")
 const form = document.getElementById("channel-form")
 const formInput = document.getElementById("channel-id") as HTMLInputElement
 
+const serverMsg = document.getElementById("server-message")
+
 form?.addEventListener("submit", (e) => {
     e.preventDefault()
     socket.emit("join-channel", formInput?.value)
 })
 
 socket.on("failure", (failureMsg) => {
-    console.log(failureMsg)
+    if (serverMsg) {
+        serverMsg.style.display = "block"
+        serverMsg.innerHTML = failureMsg
+    }
 })
 
-socket.on("success", (successMsg) => {
-    console.log(successMsg)
+socket.on("success", () => {
+    if (serverMsg) {
+        serverMsg.style.display = "none"
+    }
 })
