ede1298544780d44ff2de16eb6178bba9f5aea5b
RayanEreteo <rayabf5@gmail.com>
2026-02-05 13:01:42 +0100

    minor change

--- src/room.ts
+++ src/room.ts
@@ -1,12 +1,11 @@
 import io from "socket.io-client"
 
 const socket = io("http://localhost:5000")
-
 const channelIdText = document.getElementById("id-text") as HTMLHeadingElement
 const channelID: string | null = localStorage.getItem("channelID")
 
-if (channelID == null) {
-    window.location.href = "/"p
+if (!channelID) {
+    window.location.href = "/"
 }
 
-channelIdText.innerHTML = `Channel ID : ${channelID}` 
+channelIdText.innerHTML = `Channel ID : ${channelID}`
