5ed813fdd5fbc2869cb895baa387daf6b6858ad2
RayanEreteo <rayabf5@gmail.com>
2026-02-11 15:56:48 +0100

    Removed TODO

--- src/room.ts
+++ src/room.ts
@@ -1,6 +1,3 @@
-// TODO : need size checking for backend
-//! BUGS : Sometimes the image is not send for unknown reason.
-
 import io from "socket.io-client"
 
 const socket = io("http://192.168.1.221:5000")
